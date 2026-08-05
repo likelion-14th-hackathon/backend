a6ea5a02aab5e71ea33e65b6d4431ef124ed6e7d
김민주 <jasminekim0419@gmail.com>
2026-08-06 00:26:09 +0900

    Initial project setup

---
+++ src/main/java/com/example/likelion14th_hackathon/Likelion14thHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion14th_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion14thHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Likelion14thHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelion14th_hackathon/Likelion14thHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion14th_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion14thHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

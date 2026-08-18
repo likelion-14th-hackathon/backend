186bea030ab3ba8882841fac68ba3dc722627378
minjae <minjaec1030@gmail.com>
2026-08-19 00:28:55 +0900

    feat.프론트 연동 관련 추가

--- src/main/java/com/example/likelion14th_hackathon/common/config/WebConfig.java
+++ src/main/java/com/example/likelion14th_hackathon/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.likelion14th_hackathon.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,18 @@ public class WebConfig implements WebMvcConfigurer {
         // /uploads/** 요청 → 실제 저장 폴더의 파일로 연결
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
+    }
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }

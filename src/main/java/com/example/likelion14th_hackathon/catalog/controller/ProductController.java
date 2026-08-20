020d36c989c0562100a305c488ec92ae54b72114
JYPxxx <qkrwlsdud2400@gmail.com>
2026-08-20 14:34:18 +0900

    Support API product paths

--- src/main/java/com/example/likelion14th_hackathon/catalog/controller/ProductController.java
+++ src/main/java/com/example/likelion14th_hackathon/catalog/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping({"/products", "/api/products"})
 @RequiredArgsConstructor
 public class ProductController {
 

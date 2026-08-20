3420b7d1284b367d26b5e7eb23f7dd517d78095a
JYPxxx <qkrwlsdud2400@gmail.com>
2026-08-20 15:14:02 +0900

    Support auth member lookup path

--- src/main/java/com/example/likelion14th_hackathon/member/presentation/MemberController.java
+++ src/main/java/com/example/likelion14th_hackathon/member/presentation/MemberController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/members", "/api/members"})
+@RequestMapping({"/members", "/api/members", "/auth/members"})
 public class MemberController {
 
     private final MemberService memberService;
